c0305872f47a14cb6e8466e67609b0e30e3f9246
Muhammad Fadlyas <mhmmdfdlyas@proton.me>
2025-10-05 09:16:05 +0700

    Update get_clang.sh

--- get_clang.sh
+++ get_clang.sh
@@ -5,7 +5,7 @@ DIR="$(pwd)"
 mkdir -p "$DIR/greenforce-clang"
 
 source <(curl -sL https://raw.githubusercontent.com/greenforce-project/greenforce_clang/refs/heads/main/get_latest_url.sh) && \
-    wget -O - "$LATEST_URL_GZ" | tar -xz -C "$DIR/greenforce-clang"
+    wget -O - "$LATEST_URL" | tar -xz -C "$DIR/greenforce-clang"
 
 if [[ ! -e "$DIR/greenforce-clang/bin/clang" ]]; then
     echo "Error: Clang not found." && exit 1
