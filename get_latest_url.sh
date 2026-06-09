0ec81ac8d1cf17eb5567aaaa8986a67c856cde89
fadlyasmuhammad-mbg <fadlyasmuhammad@gmail.com>
2026-06-10 04:56:15 +0700

    Update get_latest_url.sh

--- get_latest_url.sh
+++ get_latest_url.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-LATEST_URL=https://github.com/greenforce-project/greenforce_clang/releases/download/20260609/gf-clang-23.0.0-20260609.tar.gz
+LATEST_URL=https://github.com/greenforce-project/greenforce_clang/releases/download/20260531/gf-clang-22.1.7-20260531.tar.gz
